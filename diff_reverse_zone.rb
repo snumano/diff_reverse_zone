f385338fb15f7ce1e858da4e3024f38bf9d9664d
Shugo Numano <snumano@idcf.jp>
2015-08-20 14:59:56 +0900

    digのns指定なしのパターンを追加

--- diff_reverse_zone.rb
+++ diff_reverse_zone.rb
@@ -3,19 +3,22 @@
 # snumano 2015/08/18
 
 zone_file = "zone.txt"
-#arr_ns   = ["ns01.idc.jp", "ns01.idcfcloud.com"]
-arr_ns   = ["ns01.idcfcloud.com"]
-#arr_file = ["Result/ManagedDNS.txt", "Result/IDCFCloudDNS.txt"]
-arr_file = ["Result/IDCFCloudDNS.txt"]
+arr_ns   = ["ns01.idc.jp", "ns01.idcfcloud.com",""]
+arr_file = ["Result/ManagedDNS.txt", "Result/IDCFCloudDNS.txt","Result/default_out.txt"]
 
 arr_ns.each_with_index{|ns, i|
+  p i
   puts ns
   out_file = open("#{arr_file[i]}","w") 
   in_file = open(zone_file)
   in_file.each { |zone|
     puts zone
     zone.chomp!
-    r = `/usr/bin/dig +noall +answer ns #{zone} @#{ns}`
+    if i==2
+      r = `/usr/bin/dig +noall +answer ns #{zone}`
+    else
+      r = `/usr/bin/dig +noall +answer ns #{zone} @#{ns}`
+    end
     r.split("\n").each{|each|
       each =~ %r{^(.+.in-addr.arpa.).*NS\s*(\S+)$}
       out_file.puts $~[1] + "\t<NS>\t" + $~[2]
@@ -23,16 +26,21 @@ arr_ns.each_with_index{|ns, i|
     (0..255).each{|num|
       print "."
       STDOUT.flush
-      r =`/usr/bin/dig +noall +answer ptr #{num.to_s}.#{zone} @#{ns}`
+      if i==2
+        r =`/usr/bin/dig +noall +answer ptr #{num.to_s}.#{zone}`
+      else
+        r =`/usr/bin/dig +noall +answer ptr #{num.to_s}.#{zone} @#{ns}`
+      end
       unless r.empty?
-        r =~ %r{^(.+.in-addr.arpa.).*PTR\s*(\S+)$}
-#        puts r
 #        p r
-        out_file.puts $~[1] + "\t" + $~[2]
+        r.split("\n").sort.each{|each|
+          each =~ %r{^(.+.in-addr.arpa.).*PTR\s*(\S+)$}
+          out_file.puts $~[1] + "\t" + $~[2]
+        }
       end
     }
     puts # STDOUTに改行を挿入。見やすくするため
-#    sleep(5)    
+#    sleep(3)    
   }
   in_file.close
   out_file.close
